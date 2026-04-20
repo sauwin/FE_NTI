4bb21ac97e71630ff1860699faccf9caff417d6c
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-04-21 01:29:03 +0200

    Navbar and banner added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav class="w-screen h-12 flex justify-between px-4">
+    <div class="font-medium text-xl flex gap-2 items-center">
+      <img src="/logo.png" class="h-8 w-8">
+      Nitrianský technologický inkubátor
+    </div>
+    <div class="flex gap-4 items-center px-8 mt-1">
+      <a class="hover:text-blue-600">Home</a>
+      <a class="hover:text-blue-600">About</a>
+      <a class="hover:text-blue-600">FAQ</a>
+    </div>
+  </nav>
+  <main>
+    <img src="/banner.jpg" class="w-screen h-[15vw] object-cover object-[75%_25%]">
+  </main>
 </template>
 
 <style scoped></style>
