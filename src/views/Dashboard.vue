e8a20f55c9c4bef583f0e1e2ed53ef4ec80ba9d2
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-04-27 22:55:04 +0200

    Simple dashboard for testing purposes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,48 @@
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue'
+  import { useRouter } from 'vue-router'
+  import api from '../api/axios'
+
+  const isAuthentified = ref(false)
+  const userObj = ref<User | null>(null)
+  const router = useRouter()
+
+  type User = {
+    email: string;
+  };
+
+  async function fetchData() {
+    try {
+      const res = await api.get('/auth/me')
+      userObj.value = res.data
+      isAuthentified.value = true
+    } catch(e: any) {
+      isAuthentified.value = false
+    }
+  }
+
+  async function logout() {
+    if (!isAuthentified) return
+    try {
+      const res = await api.post('/auth/logout')
+      router.push('/login')
+    } catch {
+      alert("Enable to log out")
+    }
+  }
+
+  onMounted(() => {
+    fetchData()
+  })
+</script>
+
 <template>
   <h1 class="font-bold text-4xl text-center p-4">Dashboard</h1>
-  <div class="flex mt-5 justify-center">
-    Dashboard of user [User] of role [Role]
+  <div v-if="isAuthentified" class="flex flex-col mt-5 w-full items-center">
+    <div>Dashboard of user {{userObj?.email}}</div>
+    <button @click="logout" class=" bg-blue-700 text-white mt-5 w-40 h-10">Log out</button>
+  </div>
+  <div v-else class="flex mt-5 justify-center">
+    User is not logged in <br>
   </div>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import api from '../api/axios'
+
+  const email = ref('')
+  const password = ref('')
+  const error = ref('')
+  const router = useRouter()
+
+  async function submit() {
+    error.value = ''
+    try {
+      const res = await api.post('/auth/login', { email: email.value, password: password.value })
+      localStorage.setItem('token', res.data.token)
+      router.push('/dashboard')
+    } catch {
+      error.value = 'Invalid email or password'
+    }
+  }
+</script>
+
 <template>
   <h1 class="font-bold text-4xl text-center p-4">Login</h1>
   <div class="flex justify-center">
@@ -11,15 +33,6 @@
         <label for="password" class="block text-blue-800">Password</label>
         <input v-model="password" type="password" class="border border-black w-100 h-8" />
       </div>
-<!--      <div>
-        <label for="confirm" class="block text-blue-800">Type of account</label>
-        <select name="type" id="type" class="border border-black w-100 h-8">
-          <option value="student">Student</option>
-          <option value="company">Company</option>
-          <option value="mentor">Mentor</option>
-          <option value="internal">Internal role</option>
-        </select>
-      </div>-->
       <input type="submit" class="bg-blue-700 text-white w-100 h-10 mt-4">
       <div class = "text-center mt-4">
         Not registered? <router-link class="text-blue-600 hover:text-blue-900" to="/register">Create an account</router-link>
@@ -27,24 +40,3 @@
     </form>
   </div>
 </template>
-<script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import api from '../api/axios'
-
-const email = ref('')
-const password = ref('')
-const error = ref('')
-const router = useRouter()
-
-async function submit() {
-  error.value = ''
-  try {
-    const res = await api.post('/auth/login', { email: email.value, password: password.value })
-    localStorage.setItem('token', res.data.token)
-    router.push('/dashboard')
-  } catch {
-    error.value = 'Invalid email or password'
-  }
-}
-</script>
