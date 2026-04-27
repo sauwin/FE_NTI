6e3f827d7a2659f59573817ec07b8eb33c91c3e8
17gon <17gon@github.com>
2026-04-27 15:31:10 +0300

    Minor changes
    Add axios (test required). Changed some fields so auth can work

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,17 @@
 <template>
   <h1 class="font-bold text-4xl text-center p-4">Login</h1>
   <div class="flex justify-center">
-    <form class="flex flex-col gap-2 mt-5">
+    <form class="flex flex-col gap-2 mt-5" @submit.prevent="submit">
+      <p v-if="error" class="text-red-500 text-sm">{{ error }}</p>
       <div>
-        <label for="email" class="block text-blue-800">Email</label> 
-        <input name="email" id="email" type="email" class="border border-black w-100 h-8">
+        <label for="email" class="block text-blue-800">Email</label>
+        <input v-model="email" type="email" class="border border-black w-100 h-8" />
       </div>
       <div>
-        <label for="password" class="block text-blue-800">Password</label> 
-        <input name="password" id="password" type="password" class="border border-black w-100 h-8">
+        <label for="password" class="block text-blue-800">Password</label>
+        <input v-model="password" type="password" class="border border-black w-100 h-8" />
       </div>
-      <div>
+<!--      <div>
         <label for="confirm" class="block text-blue-800">Type of account</label>
         <select name="type" id="type" class="border border-black w-100 h-8">
           <option value="student">Student</option>
@@ -18,7 +19,7 @@
           <option value="mentor">Mentor</option>
           <option value="internal">Internal role</option>
         </select>
-      </div>
+      </div>-->
       <input type="submit" class="bg-blue-700 text-white w-100 h-10 mt-4">
       <div class = "text-center mt-4">
         Not registered? <router-link class="text-blue-600 hover:text-blue-900" to="/register">Create an account</router-link>
@@ -26,3 +27,24 @@
     </form>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import api from '../api/axios'
+
+const email = ref('')
+const password = ref('')
+const error = ref('')
+const router = useRouter()
+
+async function submit() {
+  error.value = ''
+  try {
+    const res = await api.post('/auth/login', { email: email.value, password: password.value })
+    localStorage.setItem('token', res.data.token)
+    router.push('/dashboard')
+  } catch {
+    error.value = 'Invalid email or password'
+  }
+}
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,37 +1,73 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import api from '../api/axios'
+
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const password = ref('')
+const passwordConfirm = ref('')
+const role = ref('student')
+const agreed = ref(false)
+const error = ref('')
+const router = useRouter()
+
+async function submit() {
+  error.value = ''
+  if (!agreed.value) { error.value = 'You must agree to Terms'; return }
+  try {
+    const res = await api.post('/auth/register', {
+      first_name: firstName.value,
+      last_name: lastName.value,
+      email: email.value,
+      password: password.value,
+      password_confirmation: passwordConfirm.value,
+      role: role.value,
+    })
+    localStorage.setItem('token', res.data.token)
+    router.push('/dashboard')
+  } catch (e: any) {
+    error.value = e.response?.data?.message ?? 'Registration failed'
+  }
+}
+</script>
+
 <template>
   <h1 class="font-bold text-4xl text-center p-4">Register</h1>
   <div class="flex justify-center">
-    <form class="flex flex-col gap-2 mt-5">
+    <form class="flex flex-col gap-2 mt-5" @submit.prevent="submit">
+      <p v-if="error" class="text-red-500 text-sm">{{ error }}</p>
       <div class="flex gap-4">
         <div>
-          <label for="name" class="block text-blue-800">Name</label>
-          <input name="name" id="name" type="text" class="border border-black w-48 h-8">
+          <label class="block text-blue-800">Name</label>
+          <input v-model="firstName" type="text" class="border border-black w-48 h-8" />
         </div>
 
         <div>
-          <label for="surname" class="block text-blue-800">Surname</label> 
-          <input name="surname" id="surname" type="text" class="border border-black w-48 h-8">
+          <label class="block text-blue-800">Surname</label>
+          <input v-model="lastName" type="text" class="border border-black w-48 h-8" />
         </div>
       </div>
 
       <div>
-        <label for="email" class="block text-blue-800">Email</label> 
-        <input name="email" id="email" type="email" class="border border-black w-100 h-8">
+        <label class="block text-blue-800">Email</label>
+        <input v-model="email" type="email" class="border border-black w-100 h-8" />
       </div>
 
       <div>
-        <label for="password" class="block text-blue-800">Password</label> 
-        <input name="password" id="password" type="password" class="border border-black w-100 h-8">
+        <label class="block text-blue-800">Password</label>
+        <input v-model="password" type="password" class="border border-black w-100 h-8" />
       </div>
 
       <div>
-        <label for="confirm" class="block text-blue-800">Confirm password</label> 
-        <input name="confirm" id="confirm" type="password" class="border border-black w-100 h-8">
+        <label class="block text-blue-800">Confirm password</label>
+        <input v-model="passwordConfirm" type="password" class="border border-black w-100 h-8" />
       </div>
 
       <div>
-        <label for="confirm" class="block text-blue-800">Type of account</label>
-        <select name="type" id="type" class="border border-black w-100 h-8">
+        <label class="block text-blue-800">Type of account</label>
+        <select v-model="role" class="border border-black w-100 h-8">
           <option value="student">Student</option>
           <option value="company">Company</option>
           <option value="mentor">Mentor</option>
@@ -40,7 +76,7 @@
       </div>
 
       <div class="mt-2">
-        I read and agree to Terms and conditions <input type="checkbox" class="pt-8 align-middle">
+        I read and agree to Terms and conditions <input v-model="agreed" type="checkbox" class="align-middle" />
       </div>
 
       <input type="submit" class="bg-blue-700 text-white w-100 h-10 mt-4">
