f023754fa10308eeae6bede34a1fc57bba580a1a
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-04-29 15:42:52 +0200

    Program A design changes

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center gap-20">
-    <div class="w-100 h-120 bg-gray-100 flex flex-col items-center">
+    <div class="w-100 h-120 bg-blue-950 flex flex-col items-center">
       <h2 class="text-xl p-4">Nový strategický partnér</h2>
       <img src="/Dominik-Halvonik.png" class="w-[80%] h-[60%] object-cover">
       <div class="px-8 py-4">
@@ -8,7 +8,7 @@
       </div>
     </div>
 
-    <div class="w-100 h-120 bg-gray-100 flex flex-col items-center">
+    <div class="w-100 h-120 bg-blue-950 flex flex-col items-center">
       <h2 class="text-xl p-4">Nový strategický partnér</h2>
       <img src="/Dominik-Halvonik.png" class="w-[80%] h-[60%] object-cover">
       <div class="px-8 py-4">
@@ -16,7 +16,7 @@
       </div>
     </div>
 
-    <div class="w-100 h-120 bg-gray-100 flex flex-col items-center">
+    <div class="w-100 h-120 bg-blue-950 flex flex-col items-center">
       <h2 class="text-xl p-4">Nový strategický partnér</h2>
       <img src="/Dominik-Halvonik.png" class="w-[80%] h-[60%] object-cover">
       <div class="px-8 py-4">
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="px-20 py-2 mt-auto bg-gray-100 w-full">
+  <footer class="px-10 py-2 mt-auto bg-slate-800 w-full text-slate-200 text-sm">
     (c) {{ (new Date).getFullYear() }} Nitriansky technologický inkubátor 
   </footer>
 </template>
