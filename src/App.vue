de0943d7d6e60e2d34d3e06a5aa48548cb36d8de
17gon <123870086+17gon@users.noreply.github.com>
2026-05-10 17:53:30 +0300

    Notification bell
    Added notification bell. I am not great at desigh

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+import api from './api/axios'
+
+const auth = useAuthStore()
+
+onMounted(async () => {
+  if (auth.token) {
+    try {
+      const res = await api.get('/auth/me')
+      auth.login(auth.token, res.data)
+    } catch {
+      auth.logout()
+    }
+  }
+})
 </script>
 
 <template>
