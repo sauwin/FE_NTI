60a10dfd1bda90ae0f5932cc13433e04f82b50f2
17gon <123870086+17gon@users.noreply.github.com>
2026-06-06 21:49:18 +0300

    ci/cd required this

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useAuthStore } from '../../features/auth/stores/auth'
+import { useAuthStore } from '@/features/auth/stores/auth.ts'
 
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
