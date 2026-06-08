96639e607283533d27b45740b766ec7f76b513ac
Maksym Druziuk <druziukmaksym@gmail.com>
2026-06-08 21:55:22 +0200

    ContactPartners

--- src/features/partners/api/partners.ts
+++ src/features/partners/api/partners.ts
@@ -3,3 +3,7 @@ import api from '@/shared/api/axios'
 export function getPartners() {
     return api.get('/partners')
 }
+
+export function getContactMembers(partnerId: number) {
+    return api.get(`/contacts/${partnerId}`)
+}
